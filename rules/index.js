e2dd4150a007c3d2fe785a46a3b04ab955d27d0e
Vivek Singh <petmongrels@gmail.com>
2018-06-27 18:47:45 +0530

    skip logic for mother delivery form

--- rules/index.js
+++ rules/index.js
@@ -4,7 +4,7 @@ const {RuleFactory, FormElementStatusBuilder, FormElementsStatusHelper} = requir
 //Kept on hold as rules-config library is not ready yet.
 
 const MotherProgramEnrolmentFilter = RuleFactory('026e2f5c-8670-4e4b-9a54-cb03bbf3093d', 'ViewFilter');
-
+const DeliveryFilter = RuleFactory("cc6a3c6a-c3cc-488d-a46c-d9d538fcc9c2", 'ViewFilter');
 
 class MotherProgramEnrolmentHandler {
     constructor() {
@@ -18,6 +18,40 @@ class MotherProgramEnrolmentHandler {
         this.numberOfStillbirths = hideOnFirstPregnancy;
         this.numberOfChildDeaths = hideOnFirstPregnancy;
         this.numberOfLivingChildren = hideOnFirstPregnancy;
+    }
+}
+
+@DeliveryFilter('39f152ec-4b3a-4b08-b4cd-49c569d8a404', 'Skip logic for delivery form')
+class DeliveryFilterHandler {
+    static exec(programEncounter, formElementGroup, today) {
+        return FormElementsStatusHelper
+            .getFormElementsStatusesWithoutDefaults(new DeliveryFilterHandler(), programEncounter, formElementGroup, today);
+    }
+
+    constructor() {
+        this.otherPlaceOfDelivery = (programEncounter, formElement) => {
+            return DeliveryFilterHandler.encounterCodedObsHas(programEncounter, formElement, 'Place of delivery', 'Other');
+        };
+        this.whyDidYouChooseToHaveABirthAtHome = (programEncounter, formElement) => {
+            return new FormElementStatusBuilder({programEncounter, formElement}).show().when.valueInEncounter('Place of delivery').containsAnyAnswerConceptName('Home in FB', 'Home outside FB').build();
+        };
+        this.otherReasonToHaveBirthAtHome = (programEncounter, formElement) => {
+            return DeliveryFilterHandler.encounterCodedObsHas(programEncounter, formElement, 'Reason to have birth at home', 'Other');
+        };
+        this.didYouReceiveJsy = (programEncounter, formElement) => {
+            return DeliveryFilterHandler.encounterCodedObsHas(programEncounter, formElement, 'Place of delivery', 'Government Hospital');
+        };
+        this.labourTime = (programEncounter, formElement) => {
+            return DeliveryFilterHandler.encounterCodedObsHas(programEncounter, formElement, 'Place of delivery', 'Private hospital');
+        };
+        this.dateOfDischarge = (programEncounter, formElement) => {
+            return new FormElementStatusBuilder({programEncounter, formElement}).show().when.valueInEncounter('Place of delivery').not.containsAnyAnswerConceptName('Home in FB', 'Home outside FB').build();
+        };
+    }
+
+    static encounterCodedObsHas(programEncounter, formElement, conceptName, answerConceptName) {
+        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        return statusBuilder.show().when.valueInEncounter(conceptName).containsAnswerConceptName(answerConceptName).build();
     }
 }
 
