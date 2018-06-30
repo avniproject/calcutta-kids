e40f25c5144dac67de01c09d968d119b0d1d471b
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-06-30 18:10:38 +0530

    Add Gravida as last element in GPAL in pregnancy enrolment form

--- rules/pregnancy/EnrolmentFilter.js
+++ rules/pregnancy/EnrolmentFilter.js
@@ -9,6 +9,16 @@ const MotherProgramEnrolmentFilter = RuleFactory('026e2f5c-8670-4e4b-9a54-cb03bb
 const _calculateBMI = (weight, height) => {
     return Math.ceil((weight / Math.pow(height, 2)) * 10000, 1);
 };
+const _gravidaBreakup = [
+    'Number of miscarriages',
+    'Number of Abortions',
+    'Number of stillbirths',
+    'Number of child deaths',
+    'Number of living children'
+];
+const _computeGravida = (programEnrolment) => _gravidaBreakup.map((cn) => programEnrolment.getObservationValue(cn))
+    .filter(Number.isFinite)
+    .reduce((a, b) => a + b, 0);
 
 @MotherProgramEnrolmentFilter('40202177-7142-45c1-bf70-3d3b432799c0', 'Hide non applicable questions for first pregnancy', 100.0, {})
 class HideNAFirstPregnancyQuestions {
@@ -41,6 +51,11 @@ class HideNAFirstPregnancyQuestions {
             value = _calculateBMI(weight, height);
         }
         return new FormElementStatus(formElement.uuid, true, value);
+    }
+
+    gravida(programEnrolment, formElement) {
+        const isFirstPregnancy = programEnrolment.getObservationReadableValue('Is this your first pregnancy?') === 'Yes';
+        return new FormElementStatus(formElement.uuid, true, isFirstPregnancy ? 1 : _computeGravida(programEnrolment));
     }
 
     static exec(programEncounter, formElementGroup, today) {
