48063ef39bc9ee8138608f732999271f356bed53
Vivek Singh <petmongrels@gmail.com>
2018-07-02 20:24:07 +0530

    anc home visit decisions

--- rules/pregnancy/ANCDoctorVisitHandler.js
+++ rules/pregnancy/ANCDoctorVisitHandler.js
@@ -71,7 +71,7 @@ class ANCDoctorVisitRemoveAllDecisions {
             "enrolmentDecisions": [],
             "encounterDecisions": [],
             "registrationDecisions": []
-        }
+        };
         return decisions;
     }
 }
