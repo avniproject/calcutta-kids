303995ee19cc394bc85b424929643ff517a9988e
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-03 18:34:17 +0530

    Add `Child-PNC-Skip logics`

--- rules/child/pncHandler.js
+++ rules/child/pncHandler.js
@@ -1,4 +1,8 @@
 import { RuleFactory, FormElementsStatusHelper, StatusBuilderAnnotationFactory, complicationsBuilder as ComplicationsBuilder } from 'rules-config/rules';
+import ObservationMatcherAnnotationFactory from '../ObservationMatcherAnnotationFactory';
+import RuleHelper from '../RuleHelper';
+
+const CodedObservationMatcher = ObservationMatcherAnnotationFactory(RuleHelper.Scope.Encounter, 'containsAnyAnswerConceptName')(['programEncounter', 'formElement']);
 
 const ViewFilter = RuleFactory('e09dddeb-ed72-40c4-ae8d-112d8893f18b', 'ViewFilter');
 const Decision = RuleFactory('e09dddeb-ed72-40c4-ae8d-112d8893f18b', 'Decision');
@@ -22,6 +26,60 @@ class ChildPNCFormHandler {
     anyEyeProblems([], statusBuilder) {
         statusBuilder.skipAnswers('Icterus present');
     }
+
+    @CodedObservationMatcher('PNC stool related complaints', ['Loose stools'])
+    looseMotionSinceHowManyDays() { }
+
+    @CodedObservationMatcher('Did the baby receive colostrums (first milk)?', ['No'])
+    whyDidTheBabyNotReceiveColostrums() { }
+
+    @CodedObservationMatcher('Baby did not receive colostrum because', ['Other'])
+    whatOtherReasonForBabyNotReceivingColostrum() { }
+
+    @CodedObservationMatcher('Did the baby receive anything before s/he started breastfeeding?', ['Yes'])
+    whatDidTheBabyReceiveBeforeBreastfeeding() { }
+
+    @CodedObservationMatcher('Other things baby was fed before breastfeeding', ['Other'])
+    whatOtherThingsWasBabyFedBeforeBreastfeeding() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ['Other'])
+    whatOtherThingsDidYouFeedYourChildSinceBeginningBreastfeeding() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ["Cow's milk"])
+    frequencyOfCowsMilk() { }
+
+    @CodedObservationMatcher("Frequency of cow's milk", ['Other'])
+    otherFrequencyOfCowsMilk() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ['Formula'])
+    frequencyOfFormula() { }
+
+    @CodedObservationMatcher("Frequency of formula", ['Other'])
+    otherFrequencyOfFormula() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ['Water'])
+    frequencyOfWater() { }
+
+    @CodedObservationMatcher("Frequency of water", ['Other'])
+    otherFrequencyOfWater() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ['Water based liquids (sugar water, juice etc)'])
+    frequencyOfWaterBasedLiquids() { }
+
+    @CodedObservationMatcher("Frequency of water-based liquids", ['Other'])
+    otherFrequencyOfWaterBasedLiquids() { }
+
+    @CodedObservationMatcher('Things baby was fed since beginning breastfeeding', ['Other'])
+    frequencyOfOther() { }
+
+    @CodedObservationMatcher('Frequency of other', ['Other'])
+    otherFrequencyOfOther() { }
+
+    @CodedObservationMatcher('Why did you feed your baby something other than breast milk?', ['Other'])
+    otherReasonWhyYouFedSomethingOtherThanBreastMilk() { }
+
+    @CodedObservationMatcher('Are you having any of the following breastfeeding problems?', ['Other'])
+    specifyOtherBreastfeedingProblems() { }
 
     static exec(programEncounter, formElementGroup, today) {
         return FormElementsStatusHelper
