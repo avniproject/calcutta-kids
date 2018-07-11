784d3b2f821a5e7a3dde60deb67e62aa75570237
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-11 18:35:08 +0530

    Code Format

--- rules/general/doctorVisitFollowupHandler.js
+++ rules/general/doctorVisitFollowupHandler.js
@@ -4,7 +4,7 @@ const DoctorVisitFollowupFilter = RuleFactory("db80ea16-8538-41ca-bd6a-53abc9be0
 
 class DoctorVisitFollowupHandler {
     whyIsThePatientTakingOnlySomeOfThePrescribedMedicines(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Is patient taking medicines as prescribed?")
             .containsAnswerConceptName("SOME of the prescribed medicines")
             .or.containsAnswerConceptName("NONE of the prescribed medicines")
@@ -13,42 +13,42 @@ class DoctorVisitFollowupHandler {
     }
 
     otherReasonsForTakingOnlySomePrescribedMedicines(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Reasons for patient taking only some of the prescribed medicines")
             .containsAnswerConceptName("Other");
         return statusBuilder.build();
     }
 
     inWhatWayIsThePatientTakingTheirMedicineIncorrectlyPleaseEncouragePatientToTakeTheirMedicineCorrectly(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Does patient take correct dose of medicine and complete course fully?")
             .containsAnswerConceptName("No");
         return statusBuilder.build();
     }
 
     namesOfNonPrescribedMedicinesBeingTaken(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Does patient takes medicines other than the prescribed medicine?")
             .containsAnswerConceptName("Yes");
         return statusBuilder.build();
     }
 
     whyWasThePatientReferredToTheHospital(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Was patient referred to the hospital during their visit to clinic?")
             .containsAnswerConceptName("Yes");
         return statusBuilder.build();
     }
 
     didPatientTakeReferralAdviceAndGoToHospital(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Was patient referred to the hospital during their visit to clinic?")
             .containsAnswerConceptName("Yes");
         return statusBuilder.build();
     }
 
     patientDidntGoToHospitalAsPerReferralAdviceBecause(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
         statusBuilder.show().when.valueInEncounter("Did patient take referral advice and go to hospital?")
             .containsAnswerConceptName("No");
         return statusBuilder.build();
