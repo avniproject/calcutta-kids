307ec18ca9002039abc4f164b204d0f96da02ff9
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-15 20:27:10 +0530

    Make use of `SkipLogic` builders from `rules-config` in `General/DoctorVisitFollowupHandler`

--- rules/general/doctorVisitFollowupHandler.js
+++ rules/general/doctorVisitFollowupHandler.js
@@ -1,102 +1,52 @@
-const { StatusBuilderAnnotationFactory, RuleFactory, FormElementStatusBuilder, FormElementsStatusHelper } = require('rules-config/rules');
+const { FormElementStatusBuilder, SkipLogic } = require('rules-config/rules');
+const { when, show, contains, is: equals, buildAndExport } = SkipLogic;
 
-const DoctorVisitFollowupFilter = RuleFactory("db80ea16-8538-41ca-bd6a-53abc9be088b", "ViewFilter");
-const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
-
-const pregnancyTestResult = (programEncounter) => {
+const pregnancyTestResultFunc = (programEncounter, formElement) => {
     const pregnancyTestObs = programEncounter.findLatestObservationInEntireEnrolment('Pregnancy test', programEncounter);
     return pregnancyTestObs && pregnancyTestObs.getReadableValue();
-}
+};
 
-@DoctorVisitFollowupFilter("d54dafdf-34d7-44ac-b0ca-8071486a48e9", "Doctor Visit Followup form rules", 100.0, {})
-class DoctorVisitFollowupFormRules {
-    
-    @WithStatusBuilder
-    whyIsThePatientTakingOnlySomeOfThePrescribedMedicines([], statusBuilder) {
-        statusBuilder.show().when
-            .valueInEncounter("Is patient taking medicines as prescribed?")
-            .not.containsAnyAnswerConceptName("NONE of the prescribed medicines")
-    }
+const filters = {
+    formUuid: 'db80ea16-8538-41ca-bd6a-53abc9be088b',
+    uuid: 'd54dafdf-34d7-44ac-b0ca-8071486a48e9',
+    name: 'Doctor Visit Followup form rules',
+    execOrder: 100.0,
+    otherData: {},
+    // cached: [pregnancyTestResultFunc]
+};
 
-    @WithStatusBuilder
-    forAllMedicinesThePatientIsTakingIsThePatientTakingTheMedicineCorrectlyCheckYesIfPatientIsTakingTheCorrectDoseAndCompletingTheFullCourseOfTheMedicine([], statusBuilder){
-        statusBuilder.show().when
-            .valueInEncounter("Is patient taking medicines as prescribed?")
-            .not.containsAnyAnswerConceptName("NONE of the prescribed medicines")
-    }
-
-    otherReasonsForTakingOnlySomePrescribedMedicines(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Reasons for patient taking only some of the prescribed medicines")
-            .containsAnswerConceptName("Other");
-        return statusBuilder.build();
-    }
-
-    inWhatWayIsThePatientTakingTheirMedicineIncorrectlyPleaseEncouragePatientToTakeTheirMedicineCorrectly(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Does patient take correct dose of medicine and complete course fully?")
-            .containsAnswerConceptName("No");
-        return statusBuilder.build();
-    }
-
-    namesOfNonPrescribedMedicinesBeingTaken(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Does patient takes medicines other than the prescribed medicine?")
-            .containsAnswerConceptName("Yes");
-        return statusBuilder.build();
-    }
-
-    whyWasThePatientReferredToTheHospital(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Was patient referred to the hospital during their visit to clinic?")
-            .containsAnswerConceptName("Yes");
-        return statusBuilder.build();
-    }
-
-    didPatientTakeReferralAdviceAndGoToHospital(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Was patient referred to the hospital during their visit to clinic?")
-            .containsAnswerConceptName("Yes");
-        return statusBuilder.build();
-    }
-
+filters.rules = [
+    when('Is patient taking medicines as prescribed?', contains('NONE of the prescribed medicines'))
+        .hide('whyIsThePatientTakingOnlySomeOfThePrescribedMedicines',
+            'forAllMedicinesThePatientIsTakingIsThePatientTakingTheMedicineCorrectlyCheckYesIfPatientIsTakingTheCorrectDoseAndCompletingTheFullCourseOfTheMedicine'),
+    when('Reasons for patient taking only some of the prescribed medicines', contains('Other'))
+        .show('Other reasons for taking only some prescribed medicines'),
+    when('Does patient take correct dose of medicine and complete course fully?', contains('No'))
+        .show('In what way is the patient taking their medicine incorrectly? (Please encourage patient to take their medicine correctly)'),
+    when('Does patient takes medicines other than the prescribed medicine?', contains('Yes'))
+        .show('namesOfNonPrescribedMedicinesBeingTaken'),
+    when('Was patient referred to the hospital during their visit to clinic?', contains('Yes'))
+        .show('whyWasThePatientReferredToTheHospital',
+            'didPatientTakeReferralAdviceAndGoToHospital'),
+    when(pregnancyTestResultFunc, equals('Positive'))
+        .show('doYouWantToKeepYourChild'),
+    when(pregnancyTestResultFunc, equals('Positive'))
+        .and(when('Do you want to keep your child?', contains('Yes')))
+        .show('Encourage woman to register her pregnancy in office'),
+    when(pregnancyTestResultFunc, equals('Positive'))
+        .and(when('Do you want to keep your child?', contains('No')))
+        .show('doYouPlanToGoToAHospitalToPursueAnAbortion'),
+    when(pregnancyTestResultFunc, equals('Positive'))
+        .and(when('Do you plan to go to a hospital to pursue an abortion?', contains('No')))
+        .show('counselAgainstHomeAbortionKitsAndEncourageToVisitTheHospitalForAnAbortion')
+];
+filters.directFunctions = {
     patientDidntGoToHospitalAsPerReferralAdviceBecause(programEncounter, formElement) {
-        let statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
-        statusBuilder.show().when.valueInEncounter("Did patient take referral advice and go to hospital?")
-            .containsAnswerConceptName("No");
+        const statusBuilder = new FormElementStatusBuilder({ programEncounter, formElement });
+        statusBuilder.show().when.valueInEncounter('Did patient take referral advice and go to hospital?').is.no;
         return statusBuilder.build();
-    }
-
-    @WithStatusBuilder
-    doYouWantToKeepYourChild([programEncounter, formElement], statusBuilder) {
-        statusBuilder.show().whenItem(pregnancyTestResult(programEncounter)).is.equals('Positive')
-    }
-
-    @WithStatusBuilder
-    encourageWomanToRegisterHerPregnancyInOffice([programEncounter, formElement], statusBuilder) {
-        statusBuilder.show()
-            .whenItem(pregnancyTestResult(programEncounter)).is.equals('Positive')
-            .and.valueInEncounter('Do you want to keep your child?').is.yes;
-    }
-
-    @WithStatusBuilder
-    doYouPlanToGoToAHospitalToPursueAnAbortion([programEncounter, formElement], statusBuilder) {
-        statusBuilder.show()
-            .whenItem(pregnancyTestResult(programEncounter)).is.equals('Positive')
-            .and.valueInEncounter('Do you want to keep your child?').is.no;
-    }
-
-    @WithStatusBuilder
-    counselAgainstHomeAbortionKitsAndEncourageToVisitTheHospitalForAnAbortion([programEncounter, formElement], statusBuilder) {
-        statusBuilder.show()
-            .whenItem(pregnancyTestResult(programEncounter)).is.equals('Positive')
-            .and.valueInEncounter('Do you plan to go to a hospital to pursue an abortion?').is.no;
-    }
-
-    static exec(programEncounter, formElementGroup, today) {
-        return FormElementsStatusHelper
-            .getFormElementsStatusesWithoutDefaults(new DoctorVisitFollowupFormRules(), programEncounter, formElementGroup, today);
     }
 }
 
-module.exports = { DoctorVisitFollowupFormRules };
+buildAndExport(filters, exports);
+// exports[DecisionsClass] = DecisionsClass;
