6d2f2d78916b251eabce3c0b88d5e65780b5df52
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-24 19:10:41 +0530

    Add inline ANC Advice for underweight in `pregnancy/ANC GMP` form

--- rules/pregnancy/ANCGmpHandler.js
+++ rules/pregnancy/ANCGmpHandler.js
@@ -1,12 +1,24 @@
-import { RuleFactory, complicationsBuilder as ComplicationsBuilder } from 'rules-config/rules';
+import { RuleFactory, FormElementsStatusHelper, StatusBuilderAnnotationFactory, complicationsBuilder as ComplicationsBuilder } from 'rules-config/rules';
 import lib from '../lib';
 
+const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
+
+const isAbnormalWeightGain = (programEncounter) => {
+    const { programEnrolment, encounterDateTime } = programEncounter;
+    return !lib.calculations.isNormalWeightGain(programEnrolment, programEncounter, encounterDateTime);
+}
+
+const isBelowNormalWeightGain = (programEncounter) => {
+    const { programEnrolment, encounterDateTime } = programEncounter;
+    return lib.calculations.isBelowNormalWeightGain(programEnrolment, programEncounter, encounterDateTime);
+}
+
 const pregnancyGMPDecision = RuleFactory("4632c1f5-59cd-4e65-899c-beb2c87a3bff", "Decision");
+const viewFilters = RuleFactory("4632c1f5-59cd-4e65-899c-beb2c87a3bff", 'ViewFilter');
 
 @pregnancyGMPDecision("47b05f6b-dac4-456f-8878-7ccef7cf365e", "ANC GMP decisions [CK]", 100.0, {})
 class GMPDecision {
     static highRisks(programEncounter) {
-        const { programEnrolment, encounterDateTime } = programEncounter;
         const complicationsBuilder = new ComplicationsBuilder({
             programEncounter: programEncounter,
             complicationsConcept: 'High Risk Conditions'
@@ -15,14 +27,11 @@ class GMPDecision {
         complicationsBuilder.addComplication("Irregular weight gain")
             .when.valueInEncounter("Weight").lessThanOrEqualTo(35);
 
-        complicationsBuilder.addComplication("Diet Advice Do's")
-            .when.valueInEncounter("Weight").lessThanOrEqualTo(35);
-
         complicationsBuilder.addComplication("Irregular weight gain")
-            .whenItem(lib.calculations.isNormalWeightGain(programEnrolment, programEncounter, encounterDateTime)).is.not.truthy;
+            .whenItem(isAbnormalWeightGain(programEncounter)).is.truthy;
 
         complicationsBuilder.addComplication('Intrauterine Growth Retardation')
-            .whenItem(lib.calculations.isNormalWeightGain(programEnrolment, programEncounter, encounterDateTime)).is.not.truthy;
+            .whenItem(isAbnormalWeightGain(programEncounter)).is.truthy;
 
         return complicationsBuilder.getComplications();
     }
@@ -35,7 +44,7 @@ class GMPDecision {
         });
 
         complicationsBuilder.addComplication("Irregular weight gain")
-            .whenItem(lib.calculations.isNormalWeightGain(programEnrolment, programEncounter, encounterDateTime)).is.not.truthy;
+            .whenItem(isAbnormalWeightGain(programEncounter)).is.truthy;
 
         return complicationsBuilder.getComplications();
     }
@@ -47,4 +56,21 @@ class GMPDecision {
     }
 }
 
+const FiltersUuid = 'adf2d64b-4c2d-4706-96c6-946d6a49cd87';
+@viewFilters(FiltersUuid, 'ANC GMP Visit Filter rules', 100, {}, FiltersUuid)
+class Filters {
+
+    @WithStatusBuilder
+    ckCompleteDietAdvice([encounter], statusBuilder) {
+        let belowNormalWeightGain = isBelowNormalWeightGain(encounter) && encounter.getObservationValue("Weight") != null;
+        statusBuilder.show().whenItem(belowNormalWeightGain).is.truthy;
+    }
+
+    static exec(programEncounter, formElementGroup, today) {
+        return FormElementsStatusHelper
+            .getFormElementsStatusesWithoutDefaults(new Filters(), programEncounter, formElementGroup, today);
+    }
+}
+
 module.exports = { GMPDecision };
+module.exports[FiltersUuid] = Filters;
