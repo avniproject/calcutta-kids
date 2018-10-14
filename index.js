04490b3b3c32c69c68e7f0e8c71d20c6f232f70d
Vivek Singh <petmongrels@gmail.com>
2018-10-14 19:35:41 +0530

    upgraded to latest version of rules config because deploy_rules should use user name instead of org name fixed test user's catchment

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const {postAllRules} = require("rules-config/infra");
 const _ = require('lodash');
 const serverurl = _.isEmpty(process.argv[2]) ? 'http://localhost:8021': process.argv[2];
 const token = _.isEmpty(process.argv[3]) ? 'DUMMY': process.argv[3];
-postAllRules("Calcutta Kids", "./rules/index.js", serverurl, token);
+postAllRules("ck-admin", "./rules/index.js", serverurl, token);
