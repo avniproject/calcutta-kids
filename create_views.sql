6834ab6e97b154538a4951d3f7751da5c7a59185
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-10-31 12:19:34 +0530

    Update 'Is away at village View'

--- create_views.sql
+++ create_views.sql
@@ -1,9 +1,12 @@
-create or replace view calcutta_kids_individual_away_status as
-(select i.id,
+drop view if exists calcutta_kids_individual_away_status;
+
+create view calcutta_kids_individual_away_status as
+
+(select i.id individual_id,
        encounter.cancel_observations @>
        '{"739f9a56-c02c-4f81-927b-69842d78c1e8":"5b30a5b5-8ab5-4643-a424-2992f2b8df11"}' as is_away
-from individual i
-       left join program_enrolment enrolment on i.id = enrolment.individual_id
-       left join latest_program_encounter encounter on enrolment.id = encounter.program_enrolment_id);
+from latest_program_encounter encounter
+      inner join program_enrolment enrolment on enrolment.id = encounter.program_enrolment_id
+      right join individual i on enrolment.individual_id = i.id);
 
 GRANT ALL ON ALL TABLES IN SCHEMA public TO calcutta_kids;
