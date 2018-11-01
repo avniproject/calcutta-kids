742cca4eb50fcd61c31420e280180ac4d11f0264
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-11-01 11:05:05 +0530

    Update `Is away at village View`

--- create_views.sql
+++ create_views.sql
@@ -3,8 +3,9 @@ drop view if exists calcutta_kids_individual_away_status;
 create view calcutta_kids_individual_away_status as
 
 (select i.id individual_id,
-       encounter.cancel_observations @>
-       '{"739f9a56-c02c-4f81-927b-69842d78c1e8":"5b30a5b5-8ab5-4643-a424-2992f2b8df11"}' as is_away
+      case when encounter.cancel_observations @>
+       '{"739f9a56-c02c-4f81-927b-69842d78c1e8":"5b30a5b5-8ab5-4643-a424-2992f2b8df11"}' = true
+       then 'Yes' else 'No' end as is_away
 from latest_program_encounter encounter
       inner join program_enrolment enrolment on enrolment.id = encounter.program_enrolment_id
       right join individual i on enrolment.individual_id = i.id);
