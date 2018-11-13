66310c3ff720309bb3c65e30b3c334b0b22b35db
mihirk <mihir.khatwani@gmail.com>
2018-11-13 08:31:20 +0530

    Fixing the BP Concepts as per requirements

--- rules/general/DoctorVisitFormHandler.js
+++ rules/general/DoctorVisitFormHandler.js
@@ -19,14 +19,6 @@ class DoctorVisitFormHandler {
         return this._showOnlyForMother(formElement, programEncounter);
     };
 
-    systolic(programEncounter, formElement) {
-        return this._showOnlyForMother(formElement, programEncounter);
-    }
-
-    diastolic(programEncounter, formElement) {
-        return this._showOnlyForMother(formElement, programEncounter);
-    }
-
     _showOnlyForMother(formElement, programEncounter) {
         return new FormElementStatus(formElement.uuid, programEncounter.programEnrolment.program.name === 'CK Mother');
     }
