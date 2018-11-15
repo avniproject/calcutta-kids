7b7841306235c8119b9440af22afb7e761c22503
Mihir <mihirk@users.noreply.github.com>
2018-11-15 12:59:40 +0530

    Update create_views.sql

--- create_views.sql
+++ create_views.sql
@@ -13,10 +13,11 @@ from latest_program_encounter encounter
 drop view if exists active_individuals;
 
 create or replace view active_individuals as
-select distinct i.* from program_encounter pe
-      inner join program_enrolment e on pe.program_enrolment_id = e.id
-      inner join individual i on e.individual_id=i.id
-      where pe.encounter_date_time > current_timestamp - interval '5 months 1 seconds' or e.enrolment_date_time > current_timestamp - interval '5 months 1 seconds'
-      group by e.individual_id, i.id;
+select i.* from individual i
+                    left outer join program_enrolment enrolment on i.id = enrolment.individual_id
+                    left outer join program_encounter encounter on enrolment.id = encounter.program_enrolment_id
+  where (encounter.encounter_date_time > (current_timestamp - interval '5 months 1 seconds')
+           or enrolment.enrolment_date_time > (current_timestamp - interval '5 months 1 seconds'))
+  group by i.id;
 
 GRANT ALL ON ALL TABLES IN SCHEMA public TO calcutta_kids;
