88515f906cdba03f251bf96ee86f200821394ff7
Arjun <arjunk@thoughtworks.com>
2019-03-06 13:16:59 +0530

    1076 | Renamed Abortion complaints to Post abortion complaints

--- rules/pregnancy/PostAbortionHandler.js
+++ rules/pregnancy/PostAbortionHandler.js
@@ -5,7 +5,7 @@ const filter = RuleFactory('a7ec8c80-edb2-4751-a5ec-498a9e0240a0', 'ViewFilter')
 @filter("bf61911e-90b6-405c-9d22-0f115a9b0671", "Post abortion form handler", 100.0, {})
 class PostAbortionHandler {
     otherAbortionComplaints(programEncounter, formElement) {
-        return RuleHelper.encounterCodedObsHas(programEncounter, formElement, 'Abortion complaints', 'Other');
+        return RuleHelper.encounterCodedObsHas(programEncounter, formElement, 'Post abortion complaints', 'Other');
     }
 
     static exec(programEncounter, formElementGroup, today) {
