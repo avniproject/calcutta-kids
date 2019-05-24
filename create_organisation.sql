b40091269a31588fdb1571c1508a69f481d2ffb8
sidtharthan <sidthunagarajan@gmail.com>
2019-05-24 13:07:19 +0530

    1407 | Resolve concepts question-answer dependencies

--- create_organisation.sql
+++ create_organisation.sql
@@ -1,10 +1,6 @@
-CREATE ROLE calcutta_kids NOINHERIT PASSWORD 'password';
-
-GRANT calcutta_kids TO openchs;
-
-GRANT ALL ON ALL TABLES IN SCHEMA public TO calcutta_kids;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO calcutta_kids;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO calcutta_kids;
+select create_db_user('calcutta_kids', 'password');
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-    SELECT 'Calcutta Kids', 'calcutta_kids', '2a0cc644-5a8f-4bc3-8483-dc1e00adaba4', id FROM organisation WHERE name = 'OpenCHS';
+SELECT 'Calcutta Kids', 'calcutta_kids', '2a0cc644-5a8f-4bc3-8483-dc1e00adaba4', id
+FROM organisation
+WHERE name = 'OpenCHS' and not exists (select 1 from organisation where name = 'Calcutta Kids');
